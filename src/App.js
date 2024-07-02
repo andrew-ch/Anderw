d8da688b90b4ffa00477d0a48c8aac3c49049f75
Anderw <andrewchen624@gmail.com>
2024-07-02 16:37:02 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
